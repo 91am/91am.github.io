fd3d799cf638dad0c97b91662f25ed093c6aa663
91am <killaek.info@gmail.com>
2024-05-09 18:41:10 +0200

    Hamburger menu animation

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const hamburgerMenu = document.getElementById("hamburger-menu")
 const modalMenu = document.getElementById("modal-menu")
+const modalMenuLinks = document.querySelectorAll("#modal-menu a")
 
-hamburgerMenu.addEventListener("click",()=>{
+function toggleModal(){
     hamburgerMenu.classList.toggle('opened');
     modalMenu.classList.toggle('opened');
     hamburgerMenu.setAttribute('aria-expanded', hamburgerMenu.classList.contains('opened'))
+}
+
+hamburgerMenu.addEventListener("click", toggleModal)
+
+modalMenuLinks.forEach((modalLinks)=>{
+    modalLinks.addEventListener("click", toggleModal)
 })
-
-
